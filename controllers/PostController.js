f2fa21e3e57be45aa726cc7cb64764e76ba8ff86
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2023-01-21 22:51:49 +0100

    Now an user can delete a post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -26,14 +26,43 @@ exports.create = async (req, res) => {
   });
 };
 
+// recibir los posts de un usuario
 exports.getUserPost = async (req, res) => {
   try {
     const posts = await Post.find({ user_id: req.params.userId });
     if (posts.length == 0) {
-      return res.status(200).json({ message: "Aún no tienes ningún post, ¡anímate a compartir algo!" });
+      return res
+        .status(200)
+        .json({
+          message: "Aún no tienes ningún post, ¡anímate a compartir algo!",
+        });
     }
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ message: err.message });
+  }
+};
+
+// Permitir al usuario eliminar un post
+exports.deletePost = async (req, res) => {
+  try {
+    const postToDelete = await Post.findById(req.params.postId);
+
+    await Post.deleteOne({ //Lo eliminamos de la colección de posts
+      _id: postToDelete._id,
+    });
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: postToDelete.user_id },
+      { $pull: { post_ids: postToDelete._id } },
+      { new: true }
+    );
+
+    res.status(200).json({
+      message: `El post ${postToDelete._id} ha sido eliminado del usuario ${postToDelete.user_id}`,
+      updatedUser,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const PostController = require("../controllers/PostController");
 
 router.post("/create", PostController.create); //crear un post
-router.get("/:userId", PostController.getUserPost); //recibir posts de un usuario
+router.get("/:userId", PostController.getUserPost); //recibir posts de un usuario concreto
+router.delete("/delete/:postId", PostController.deletePost);
+
 
 module.exports = router;
